6dc40f8f5a8d7ca64faff7adbc5b609d98fc440f
EmanuelTrisnajati <emanueltrisnajati@gmail.com>
2018-09-14 11:22:01 +0700

    Add file game.

---
+++ CobaCoba0_Unity/CobaCoba0_Unity/Assets/Scenes/Cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+    public int rotationAngle;
+
+    private Transform cubeTransform;
+
+    // Use this for initialization
+    void Start () {
+        cubeTransform = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        cubeTransform.Rotate(Vector3.up, rotationAngle * Time.deltaTime);
+	}
+}
